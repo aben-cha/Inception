bcbda8dd39f6209c0f3a9ed7a69d038f149315bc
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2024-12-23 23:12:35 +0100

    inception

---
+++ userServiceApi/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => { res.json({
+    name: 'ayoub', 
+    email: 'ayoub@gmail.com'
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
