fc665a2ee393bb025b96523645efbdf2400ca47c
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-01-14 05:00:00 +0100

    inception

---
+++ Makefile
@@ -0,0 +1,5 @@
+up:
+	cd ./srcs && docker-compose up --build
+down:
+	cd ./srcs && docker-compose down -v
+
--- srcs/requirements/mariadb/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS db_wordpress;
-CREATE USER 'aben-cha'@'%' IDENTIFIED BY 'aben-cha@@';
-GRANT ALL PRIVILEGES ON db_wordpress.* TO 'aben-cha'@'%';
-FLUSH PRIVILEGES;
